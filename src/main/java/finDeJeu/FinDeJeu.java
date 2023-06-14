8da39254fb874632f6b77b1dd8e77ac3e0050c28
valmenu59 <valmenu59@live.com>
2023-06-14 23:44:43 +0200

    Version sae 202 : Finalisation du jeu

--- src/main/java/finDeJeu/FinDeJeu.java
+++ src/main/java/finDeJeu/FinDeJeu.java
@@ -1,47 +1,79 @@
 package finDeJeu;
 
 import fx.BoutonJeu;
+import fx.ImageJeu;
 import fx.PoliceJeu;
+import javafx.geometry.HPos;
 import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 
 public class FinDeJeu extends Scene {
 
     private static final Font POLICE = PoliceJeu.creerPolice(38, true);
+    private static final String IMG_LOUP = "/ElementsJeu/loupSansCouleur.png";
+    private static final String IMG_MOUTON = "/ElementsJeu/moutonSansCouleur.png";
 
 
     public FinDeJeu(Stage stage, boolean gagne, int nbTour, int nbPlanteMangees){
-        super(new BorderPane(), 1280,720);
-        BorderPane panneau = (BorderPane) this.getRoot();
+        super(new AnchorPane(), 1280, 720);
+        AnchorPane anchorPane = (AnchorPane) this.getRoot();
+
+
         VBox vBox = new VBox();
         vBox.setSpacing(100);
         vBox.setAlignment(Pos.CENTER);
-        panneau.setCenter(vBox);
+
         ActionFinDeJeu actions = new ActionFinDeJeu();
         Text texte;
-        if (gagne){
-            texte = new Text("Félicitation, le mouton est sorti de l'enclos !\n"+
+        ImageView imageAnimal;
+
+
+        double x;
+        if (gagne) {
+            texte = new Text("Félicitation, le mouton est sorti de l'enclos !\n" +
                     "Vous avez gagné !\n\n" +
-                    "Nombre de tours : "+nbTour+"\t Nombre de plantes mangées : "+nbPlanteMangees);
+                    "Nombre de tours : " + nbTour + "\nNombre de plantes mangées : " + nbPlanteMangees);
+            imageAnimal = new ImageJeu(getClass().getResource(IMG_MOUTON).toExternalForm())
+                    .afficherImage(300, 300, true);
+            anchorPane.setStyle("-fx-background-color: #73f56c;");
+            x = 250.;
         } else {
-            texte = new Text("Oh non ! Le loup a mangé le mouton !\n"+
+            texte = new Text("Oh non ! Le loup a mangé le mouton !\n" +
                     "Vous avez perdu !\n\n" +
-                    "Nombre de tours : "+nbTour+"\t Nombre de plantes mangées : "+nbPlanteMangees);
+                    "Nombre de tours : " + nbTour + "\nNombre de plantes mangées : " + nbPlanteMangees);
+            imageAnimal = new ImageJeu(getClass().getResource(IMG_LOUP).toExternalForm())
+                    .afficherImage(300, 300, true);
+            anchorPane.setStyle("-fx-background-color: #1c1a1a;");
+            texte.setFill(Color.WHITE);
+            x = 300.;
         }
+
+        imageAnimal.setRotate(45);
+
+        // Positionnement de l'image en haut à droite
+        imageAnimal.setLayoutX(anchorPane.getWidth() - 300);
+        imageAnimal.setLayoutY(-15);
+        anchorPane.getChildren().add(imageAnimal);
+
         texte.setFont(POLICE);
         texte.setTextAlignment(TextAlignment.CENTER);
-        vBox.getChildren().add(texte);
-
         BoutonJeu boutonRetourMenu = new BoutonJeu("Retourner au menu", Color.rgb(200, 200, 200));
         actions.allerAuMenu(boutonRetourMenu, stage);
-        vBox.getChildren().add(boutonRetourMenu);
+        vBox.getChildren().addAll(texte, boutonRetourMenu);
+
+
+        AnchorPane.setTopAnchor(vBox, 200.);
+        AnchorPane.setLeftAnchor(vBox, x);
+        anchorPane.getChildren().add(vBox);
 
     }
 }
