7cda6fd4ac2a19601a2590a6ec8064d1d5f99212
valmenu59 <valmenu59@live.com>
2023-06-17 16:07:44 +0200

    Ajout du fichier du jeu compilé en .jar

---
+++ src/main/java/sae/saejavafx/App.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c)  @link https://github.com/valmenu59/SAE_JavaFX @link https://github.com/valmenu59
+ * CC BY-NC - Attribution - Partage dans les mêmes conditions - Pas d'utilisation commerciale
+ */
+
+package sae.saejavafx;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationFX.main(args);
+    }
+}
